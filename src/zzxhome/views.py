d8bcabea8e787c16dba5bb566a4f02f973691b00
lorem <l@null.local>
2024-07-05 15:36:57 +0300

    initial commit

---
+++ src/zzxhome/views.py
@@ -0,0 +1,32 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+import pathlib
+
+from visits.models import PageVisit
+
+this_dir = pathlib.Path(__file__).resolve().parent
+
+def home_page_view(request, *args, **kwargs):
+    page_qs = PageVisit.objects.filter(path=request.path)
+    qs = PageVisit.objects.all()
+    my_title = "My Page"
+    my_context = {
+        "page_title": my_title,
+        "page_visit_count": page_qs.count(),
+        "percent": (page_qs.count()/qs.count())*100,
+        "total_visit_count": qs.count(),
+    }
+    path = request.path
+    print(path)
+    html_template = "home.html"
+    PageVisit.objects.create(path=request.path)
+    return render(request, html_template, my_context)
+
+
+def about_page_view(request, *args, **kwargs):
+    my_title = "About Page"
+    my_context = {
+        "page_title": my_title
+    }
+    html_template = "about.html"
+    return render(request, html_template, my_context)
