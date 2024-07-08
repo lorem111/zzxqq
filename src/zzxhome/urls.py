315659ad7c34116d7708153b91f4274837c66509
lorem <l@null.local>
2024-07-08 16:05:49 +0300

    updated navbar

--- src/zzxhome/urls.py
+++ src/zzxhome/urls.py
@@ -20,7 +20,7 @@ from auth import views as auth_views
 from .views import home_view, about_view
 
 urlpatterns = [
-    path("", home_view),
+    path("", home_view, name='home'),
     path('helloworld/', home_view),
     path('about/', about_view),
     path('login/', auth_views.login_view),
