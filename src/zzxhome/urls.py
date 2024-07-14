fd320fb8ea432be240ab4fb870ee66ad0ed0871c
lorem <l@null.local>
2024-07-15 00:39:13 +0300

    added profiles, and pw_protected, user_only, and admin_only_view

--- src/zzxhome/urls.py
+++ src/zzxhome/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from auth import views as auth_views
-from .views import home_view, about_view
+from .views import home_view, about_view, pw_protected_view, user_only_view, admin_only_view
 
 urlpatterns = [
     path("", home_view, name='home'),
@@ -26,5 +26,9 @@ urlpatterns = [
     path('login/', auth_views.login_view),
     path('register/', auth_views.register_view),
     path('accounts/', include('allauth.urls')), #accounts/login, accounts/signup
+    path('protected/user-only', user_only_view),
+    path('protected/admin-only', admin_only_view),
+    path('protected/', pw_protected_view),
+    path('profiles/', include('profiles.urls')), 
     path('admin/', admin.site.urls),
 ]
--- src/zzxhome/views.py
+++ src/zzxhome/views.py
@@ -1,8 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.admin.views.decorators import staff_member_required
 import pathlib
+from django.conf import settings
 
 from visits.models import PageVisit
+
+LOGIN_URL = settings.LOGIN_URL
 
 this_dir = pathlib.Path(__file__).resolve().parent
 
@@ -31,3 +36,28 @@ def about_view(request, *args, **kwargs):
     print(path)
     PageVisit.objects.create(path=request.path)
     return render(request, html_template, my_context)
+
+VALID_CODE = "abc123"
+
+def pw_protected_view(request, *args, **kwargs):
+    
+    is_allowed = request.session.get("protected_page_allowed") or 0
+
+    #print(request.session.get("protected_page_allowed"), type (request.session.get("protected_page_allowed")))
+
+    if request.method == "POST":
+        user_pw_sent = request.POST.get("code") or None
+        if user_pw_sent == VALID_CODE:
+            is_allowed = 1
+            request.session["protected_page_allowed"] = 1 
+    if is_allowed:
+        return render(request, "protected/view.html", {})
+    return render(request, "protected/entry.html", {})
+
+@login_required
+def user_only_view(request, *args, **kwargs):
+    return render(request, "protected/user-only.html", {})
+
+@staff_member_required(login_url=LOGIN_URL)
+def admin_only_view(request, *args, **kwargs):
+    return render(request, "protected/admin-only.html", {})
